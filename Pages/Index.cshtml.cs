0ec49ec85207c5f796837fdf3fc2494d3197fec4
Rob <rob@bladen.me>
2021-03-19 21:47:08 +0000

    Add in code ready for financial calculations. Add in error handling.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -10,14 +10,31 @@ namespace ChangeCalculator.Pages
 {
     public class IndexModel : PageModel
     {
-
         public IndexModel()
         {
         }
 
-        public ActionResult OnPostCalculateChange()
+        /**
+         * Calculate the change given the balance and price.
+         * Returns JSON in the form:
+         * {"success":true,"result":{"coin type":quantity}}
+         */
+        public ActionResult OnPostCalculateChange(float yourBalance, float productPrice)
         {
-            return new JsonResult(new {result = "Test"});
+            if (yourBalance < 0)
+            {
+                return new JsonResult(new {success = false, message = "The entered balance must be not be below 0."});
+            }
+
+            if (productPrice < 0)
+            {
+                return new JsonResult(new {success = false, message = "The entered product price must be not be below 0."});
+            }
+
+            var response = new Dictionary<string, int>();
+
+
+            return new JsonResult(new {success = true, result = response});
         }
     }
 }
